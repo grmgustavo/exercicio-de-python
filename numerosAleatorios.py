676fbb5f12bb11a5199e1a7d96484236ab94afb7
Gustavo Rodrigues Machado <61809841+grmgustavo@users.noreply.github.com>
2021-03-11 22:32:45 -0300

    Corrigindo erros

--- numerosAleatorios.py
+++ numerosAleatorios.py
@@ -1,13 +1,14 @@
-from random
+from random import *
 
 
 nAl = randint(0,100)
+
 
 n = int (input("Informe um número de 0 a 100: "))
 
 
 
 if n == nAl:
-    print("Você acertou, o seu numero era {}, o mesmo que o seu".format(nAl))
+    print("Você acertou, o numero aleatório era {}, o mesmo que o seu".format(nAl))
 else: 
     print("Você errou, o numero aleatorio era {}".format(nAl))
